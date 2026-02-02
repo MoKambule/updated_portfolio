603d3ed3b3318827018adc9b1654a755690c76db
mokambujhb024 <mokambujhb024@student.wethinkcode.co.za>
2026-02-02 22:38:11 +0200

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Certificates from "./Components/Certificates";
