9668fdef02ab2c030b654210041fbf9b2e704b2c
mokambujhb024 <mokambujhb024@student.wethinkcode.co.za>
2026-02-02 21:48:46 +0200

    made floating bobs move again

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
      "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       keyframes: {
+        blob: {
+          '0%, 100%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+        },
+      },
+      animation: {
+        blob: 'blob 15s infinite',
+      },
+    },
   },
   plugins: [],
 }
